cb08133465012ebe6ca8d5f2975292675c1302e8
Brandon Gonzalez <brajgon@gmail.com>
2024-02-15 23:11:09 -0700

    about.js

--- about.js
+++ about.js
@@ -1,14 +1,19 @@
-console.log("hello world");
+// console.log("hello world");
 
 
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	alert("Submitted Succesfully");
-	// console.log('form submit');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+///
+function giveComp() {
+	alert('Looking good, Ducky');
+}
+
+document.getElementById('rubber-ducky').addEventListener('mouseover', giveComp);
